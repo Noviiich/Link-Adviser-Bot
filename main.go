9416db6f889501315e8d742b44eba57f64f44d9d
Noviiich <nowikoff63@gmail.com>
2025-02-28 04:53:31 -0500

    add consumer, event-consumer

--- main.go
+++ main.go
@@ -4,15 +4,30 @@ import (
 	"flag"
 	"log"
 
-	"github.com/Noviiich/Link-Adviser-Bot/clients/telegram"
+	tgClient "github.com/Noviiich/Link-Adviser-Bot/clients/telegram"
+	event_consumer "github.com/Noviiich/Link-Adviser-Bot/consumer/event-consumer"
+	"github.com/Noviiich/Link-Adviser-Bot/events/telegram"
+	"github.com/Noviiich/Link-Adviser-Bot/storage/files"
 )
 
 const (
-	tgBotHost = "api.telegram.org"
+	tgBotHost   = "api.telegram.org"
+	storagePath = "files_storage"
+	batchSize   = 100
 )
 
 func main() {
-	tgClient := telegram.New(tgBotHost, mustToken())
+	eProcessor := telegram.New(
+		tgClient.New(tgBotHost, mustToken()),
+		files.New(storagePath),
+	)
+
+	log.Print("service started")
+
+	consumer := event_consumer.New(eProcessor, eProcessor, batchSize)
+	if err := consumer.Start(); err != nil {
+		log.Fatal("service is stopped", err)
+	}
 }
 
 func mustToken() string {
